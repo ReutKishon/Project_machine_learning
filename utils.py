447dedcd9fb32790a05245c117c1f4995fa02798
Rafi Dinari <rafidinary@gmail.com>
2021-07-07 00:59:59 +0300

    running naive bayes on more types

--- utils.py
+++ utils.py
@@ -4,20 +4,29 @@ from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 from sklearn_pandas import gen_features
 from sklearn_pandas import DataFrameMapper
+import numpy as np
+from sklearn.impute import SimpleImputer
 
 def read_data_from_file():
     """
     read data from healthcare-dataset-stroke-data csv file
     :return: DataFrame of the data after transformation
     """
-
     # skip first line i.e. read header first and then iterate over each row od csv as a list
     df = pd.read_csv('healthcare-dataset-stroke-data.csv')
-    colnames = df.columns
-    feature_def = gen_features(columns=df.columns,
+    le = LabelEncoder()
+    not_num_cols = ["gender", "ever_married", "work_type", "Residence_type", "smoking_status"]
+
+    imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+    feature_def = gen_features(columns=not_num_cols,
                                classes=[LabelEncoder])
     mapper5 = DataFrameMapper(feature_def)
-    df = pd.DataFrame(mapper5.fit_transform(df), columns=colnames)
+    tmp_df = pd.DataFrame(mapper5.fit_transform(df), columns=not_num_cols)
+    for col in not_num_cols:
+        df[col] = tmp_df[col].values
+    imp.fit(df)
+    tmp_arr = imp.transform(df)
+    df = pd.DataFrame(tmp_arr, columns=df.columns)
     return df
 
 
@@ -38,6 +47,7 @@ class DataHolder(metaclass=Singleton):
 
     def get_features(self, label_name):
         return self.df.loc [ :, self.df.columns != label_name ]
+
 
 
 
