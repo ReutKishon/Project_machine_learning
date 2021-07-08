4073984d976f1d05e26cbf8a5b17920ac6bc821d
reutKeyshawn <reki8611@gmail.com>
2021-07-08 13:01:21 +0300

    update to same split train and test for all models

--- classification.py
+++ classification.py
@@ -28,93 +28,70 @@ def knn(x_train, y_train, x_test, n_neighbors=71):
     return model.predict(x_test)
 
 
-def check_knn(dh, label):
+def check_knn(x_train, y_train, x_test, y_test):
     avgs_dict = {}
-    features, labels = get_features_labels(dh, label)
     splits_num = 100
-    for _ in range(splits_num):
-        x_train, x_test, y_train, y_test = split_data(features, labels)
-        for i in range(1, 73):
-            y_pred = knn(x_train, y_train, x_test, i)
-            acc_score = metrics.accuracy_score(y_test, y_pred)
-            avgs_dict[i] = avgs_dict.get(i, 0) + acc_score
+    for i in range(1, 73):
+        y_pred = knn(x_train, y_train, x_test, i)
+        acc_score = metrics.accuracy_score(y_test, y_pred)
+        avgs_dict[i] =  acc_score
     max_key = max(avgs_dict, key=avgs_dict.get)
     max_val = max(avgs_dict.values())
     print(
-        f"best accuracy for {label} is for {max_key} neighbours: {max_val / splits_num}")
+        f"best accuracy for {label} is for {max_key} neighbours: {max_val}")
 
 
-def check_decision_tree(dh, label):
+def check_decision_tree(x_train, y_train, x_test, y_test):
 
-    features, labels = get_features_labels(dh, label)
-    splits_num = 100
-    avg_gini = 0
-    avg_en = 0
-    for _ in range(splits_num):
-        x_train, x_test, y_train, y_test = split_data(features, labels)
+    # instantiate the DecisionTreeClassifier model with criterion gini index
+    clf_gini = DecisionTreeClassifier(criterion='gini')
 
-        # instantiate the DecisionTreeClassifier model with criterion gini index
-        clf_gini = DecisionTreeClassifier(criterion='gini')
+    # instantiate the DecisionTreeClassifier model with criterion gini index
+    clf_en = DecisionTreeClassifier(criterion='entropy')
+    # Train Decision Tree Classifer
+    clf_gini.fit(x_train, y_train)
+    clf_en.fit(x_train, y_train)
 
-        # instantiate the DecisionTreeClassifier model with criterion gini index
-        clf_en = DecisionTreeClassifier(criterion='entropy')
-        # Train Decision Tree Classifer
-        clf_gini.fit(x_train, y_train)
-        clf_en.fit(x_train, y_train)
+    # Predict the response for test dataset
+    y_pred_gini = clf_gini.predict(x_test)
+    y_pred_en = clf_en.predict(x_test)
 
-        # Predict the response for test dataset
-        y_pred_gini = clf_gini.predict(x_test)
-        y_pred_en = clf_en.predict(x_test)
-
-        avg_gini += metrics.accuracy_score(y_test, y_pred_gini)
-        avg_en += metrics.accuracy_score(y_test, y_pred_en)
-    max_criterion = "gini" if (avg_gini > avg_en) else "entropy"
+    accuracy_gini = metrics.accuracy_score(y_test, y_pred_gini)
+    accuracy_en = metrics.accuracy_score(y_test, y_pred_en)
+    max_criterion = "gini" if (accuracy_gini > accuracy_en) else "entropy"
 
     print(
-        f"decision tree: accuracy for {label} with gini criterion: {avg_gini/splits_num}, with entropy criterion: {avg_en/splits_num}, best perfomance: {max_criterion}   ")
+        f"decision tree: accuracy for {label} with gini criterion: {accuracy_gini}, with entropy criterion: {accuracy_en}, best perfomance: {max_criterion}   ")
 
 
-def check_random_forest(dh, label):
+def check_random_forest(x_train, y_train, x_test, y_test):
 
-    features, labels = get_features_labels(dh, label)
-    splits_num = 100
-    accuracy_score = 0
+    # Create a Gaussian Classifier
+    clf = RandomForestClassifier(n_estimators=100)
 
-    for _ in range(splits_num):
-        x_train, x_test, y_train, y_test = split_data(features, labels)
+    # Train the model using the training sets y_pred=clf.predict(X_test)
+    clf.fit(x_train, y_train)
 
-        # Create a Gaussian Classifier
-        clf = RandomForestClassifier(n_estimators=100)
-
-        # Train the model using the training sets y_pred=clf.predict(X_test)
-        clf.fit(x_train, y_train)
-
-        # prediction on test set
-        y_pred = clf.predict(x_test)
-        accuracy_score += metrics.accuracy_score(y_test, y_pred)
+    # prediction on test set
+    y_pred = clf.predict(x_test)
+    accuracy_score = metrics.accuracy_score(y_test, y_pred)
 
     print(
-        f"Random forest accuracy for {label} is {accuracy_score/splits_num}")
+        f"Random forest accuracy for {label} is {accuracy_score}")
 
 
-def check_naive_bayes(dh, label):
-    totals = {"gaussian": 0, "categorial": 0, "multinomial": 0}
-    features, labels = get_features_labels(dh, label)
-    splits_num = 100
-    for _ in range(splits_num):
-        x_train, x_test, y_train, y_test = split_data(features, labels)
-        gnb = GaussianNB()
-        y_pred = gnb.fit(x_train, y_train).predict(x_test)
-        acc_score = metrics.accuracy_score(y_test, y_pred)
-        totals["gaussian"] += acc_score
-        clf = MultinomialNB()
-        y_pred = clf.fit(x_train, y_train).predict(x_test)
-        acc_score = metrics.accuracy_score(y_test, y_pred)
-        totals["multinomial"] += acc_score
-    max_key = max(totals, key=totals.get)
-    max_val = max(totals.values())
+def check_naive_bayes(x_train, y_train, x_test, y_test):
+    gnb = GaussianNB()
+    y_pred = gnb.fit(x_train, y_train).predict(x_test)
+    gaussian_score = metrics.accuracy_score(y_test, y_pred)
+    clf = MultinomialNB()
+    y_pred = clf.fit(x_train, y_train).predict(x_test)
+    multinomial_score = metrics.accuracy_score(y_test, y_pred)
+    max_key = "gaussian" if (
+        gaussian_score > multinomial_score) else "multinomial"
+    max_val = max(gaussian_score, multinomial_score)
     print(
-        f"avg naive bayes accuracy for {label} is {max_key} : {max_val / splits_num}")
+        f"avg naive bayes accuracy for {label} is {max_key} : {max_val}")
 
 
 def get_features_labels(dh, label):
@@ -127,14 +104,19 @@ def get_features_labels(dh, label):
 if __name__ == "__main__":
 
     dh = DataHolder()
+
     for label in ["hypertension", "heart_disease", "stroke"]:
-        check_knn(dh, label)
-        check_naive_bayes(dh, label)
-        check_decision_tree(dh, label)
-        check_random_forest(dh, label)
+        features, labels = get_features_labels(dh, label)
+        x_train, x_test, y_train, y_test = split_data(features, labels)
+
+        check_knn(x_train, y_train, x_test, y_test)
+        check_naive_bayes(x_train, y_train, x_test, y_test)
+        check_decision_tree(x_train, y_train, x_test, y_test)
+        check_random_forest(x_train, y_train, x_test, y_test)
 
 
 # Challenges: five of the dataset fields are strings.
 # In order to implemement knn algorithm we need to calculate Euclidian distance
 # between some sample and it's neighbors. hence all of the fields should be numarical.
+# Knn : the classification result depends on choosing k and that's the difficult task in knn.
 # 5110 samples (in knn: k = 71)
