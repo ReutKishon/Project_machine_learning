42331f76b832afe00d093cdb0ce5667dd28cabfc
Rafi Dinari <rafidinary@gmail.com>
2021-07-11 01:41:34 +0300

    adding algos

--- classification.py
+++ classification.py
@@ -1,12 +1,16 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB, CategoricalNB, MultinomialNB
 from sklearn import metrics
+from sklearn.svm import SVC
+
 from utils import *
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
 # searching for the best k for knn algo
-tests_num = 100
+tests_num = 3
 
 def check_best_k_for_knn(features, labels):
     options_for_k = {k: 0 for k in range(1, 73)}
@@ -92,6 +96,19 @@ def check_random_forest(x_train, y_train, x_test, y_test):
     accuracy_score = metrics.accuracy_score(y_test, y_pred)
     return accuracy_score
 
+def check_logistic_regression(x_train, y_train, x_test, y_test):
+
+    # Create a Gaussian Classifier
+    log_reg = LogisticRegression()
+    # Train the model using the training sets y_pred=clf.predict(X_test)
+    log_reg.fit(x_train, y_train)
+
+    # prediction on test set
+    y_pred = log_reg.predict(x_test)
+    accuracy_score = metrics.accuracy_score(y_test, y_pred)
+    return accuracy_score
+
+
 
 def best_option_for_naive_bayes(features, labels):
 
@@ -122,12 +139,25 @@ def check_naive_bayes(x_train, y_train, x_test, y_test, option):
     return accuracy_score
 
 
+def check_svm(x_train, y_train, x_test, y_test):
+    # SVM
+    # Model validation basis GridSearch Cross validation
+    param_grid = {'C': [ 0.1, 1, 10, 100, 1000, 2000 ], 'gamma': [ 1, 0.1, 0.01, 0.001, 0.0001 ], 'kernel': [ 'rbf' ]}
+    grid = GridSearchCV(SVC(), param_grid, refit=True)
+    y_pred = grid.fit(x_train, y_train).predict(x_test)
+    accuracy_score = metrics.accuracy_score(y_test, y_pred)
+    return accuracy_score
+
+
 def run_ml_project():
+    mark = 0
     dh = DataHolder()
     for label in [ "hypertension", "heart_disease", "stroke" ]:
-        best_performence_algo = {'knn': 0, 'naive_bayes': 0,
-                                 'decision_tree': 0, 'random_forest': 0}
+        best_performence_algo = {'knn': 0, 'naive_bayes': 0, "svm":0,
+                                 'decision_tree': 0, 'random_forest': 0, 'logistic_regression':0}
         features, labels = get_features_labels(dh, label)
+        features = select_features(features, labels)
+
         k = check_best_k_for_knn(features, labels)
         print(f"all follwing results are for {label}")
         print(f"best k for knn algo is: {k}")
@@ -147,6 +177,11 @@ def run_ml_project():
                 x_train, y_train, x_test, y_test, criterion)
             best_performence_algo [ 'random_forest' ] += check_random_forest(
                 x_train, y_train, x_test, y_test)
+            best_performence_algo [ 'logistic_regression' ] += check_logistic_regression(
+                x_train, y_train, x_test, y_test)
+            best_performence_algo [ 'svm' ] += check_svm(
+                x_train, y_train, x_test, y_test)
+
 
         max_algo = max(best_performence_algo,
                        key=best_performence_algo.get)
@@ -154,7 +189,9 @@ def run_ml_project():
         # Iterating over values
         for key, val in best_performence_algo.items():
             print(key, "accuracy:", val / tests_num)
-        print(f"{max_algo} has the best performances, with {round(max_perf, 2)}% accuracy!")
+        print(f"{max_algo} has the best performances, with {round(100*(max_perf/tests_num), 2)}% accuracy!")
+        mark += max_perf/tests_num
+    print(f"mark for run: {100*mark/3}%")
 
 
 if __name__ == "__main__":
